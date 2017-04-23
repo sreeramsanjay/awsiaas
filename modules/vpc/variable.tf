d198932b9878fd28791f5b5b91a6d1afd55da0e3
sreeramsanjay <sreeramsprt@gmail.com>
2017-04-22 21:58:29 -0400

    Update variable.tf

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -1,30 +1,9 @@
 variable "vpc_cidr_block" {}
 variable "project" {}
-variable "dmz_count" {
-  default = "2"
-}
-variable "priv_count" {
-  default = "2"
-}
-variable "pub_count" {
-  default = "2"
-}
-variable "availability_zones" {
-  default = ["us-east-1c", "us-east-1d"]
-}
-variable "vpc_cidr" {
-  description = "CIDR for the whole VPC"
-  default = "10.220.16.0/22"
-}
-variable "dmz_cidr" {
-  description = "CIDR for the Dmz Subnet"
-  default = ["10.220.17.128/25", "10.220.19.128/25"]
-}
-variable "priv_cidr" {
-  description = "CIDR for the Private Subnet"
-  default = ["10.220.16.0/24", "10.220.18.0/24"]
-}
-variable "pub_cidr" {
-  description = "CIDR for the Public agent Subnets"
-  default = ["10.220.17.0/25", "10.220.19.0/25"]
-}
+variable "dmz_count" {}
+variable "priv_count" {}
+variable "pub_count" {}
+variable "availability_zones" { type = "list" }
+variable "dmz_cidr" { type = "list" }
+variable "priv_cidr" { type = "list" }
+variable "pub_cidr" { type = "list" }
